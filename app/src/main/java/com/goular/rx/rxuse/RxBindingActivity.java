8abf3dcf1150966ae1f6227bd83c7d6d62801b54
Goular <zhaojt_cs@126.com>
2017-04-09 01:41:48 +0800

    添加点击按钮防止抖动的使用

--- app/src/main/java/com/goular/rx/rxuse/RxBindingActivity.java
+++ app/src/main/java/com/goular/rx/rxuse/RxBindingActivity.java
@@ -1,6 +1,8 @@
 package com.goular.rx.rxuse;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,11 +12,14 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.goular.rx.R;
+import com.goular.rx.rxuse.fragments.AutoTextFragment;
+import com.goular.rx.rxuse.fragments.CheckboxFragment;
+import com.goular.rx.rxuse.fragments.SimpleClickFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RxBindingActivity extends AppCompatActivity {
+public class RxBindingActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
     @BindView(R.id.rxbinding_toolbar)
     Toolbar rxbindingToolbar;
@@ -35,6 +40,7 @@ public class RxBindingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rx_binding);
         ButterKnife.bind(this);
         initToolBar();
+        mRadioGroup.setOnCheckedChangeListener(this);
     }
 
     private void initToolBar() {
@@ -47,5 +53,26 @@ public class RxBindingActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.rxbinding_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+        switch (i) {
+            case R.id.simple_click_btn:
+                setFragment(SimpleClickFragment.getInstance());
+                break;
+            case R.id.auto_text_btn:
+                setFragment(AutoTextFragment.getInstance());
+                break;
+            case R.id.checkbox_btn:
+                setFragment(CheckboxFragment.getInstance());
+                break;
+        }
+    }
+
+    private void setFragment(Fragment instance) {
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.mlayout, instance)
+                .commit();
     }
 }
--- app/src/main/java/com/goular/rx/rxuse/fragments/SimpleClickFragment.java
+++ app/src/main/java/com/goular/rx/rxuse/fragments/SimpleClickFragment.java
@@ -2,10 +2,25 @@ package com.goular.rx.rxuse.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.goular.rx.R;
+import com.jakewharton.rxbinding.view.RxView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import rx.Subscription;
 
 /**
  * Created by zhaoj on 2017/4/9.
@@ -14,6 +29,14 @@ import android.view.ViewGroup;
 public class SimpleClickFragment extends Fragment {
 
     private static SimpleClickFragment fragment;
+    @BindView(R.id.clicks_btn)
+    Button clicksBtn;
+    @BindView(R.id.mlistview)
+    ListView mlistview;
+    Unbinder unbinder;
+    private View rootView;
+    private List<String> mList;
+    private Subscription clickSubscription;
 
     public SimpleClickFragment() {
         super();
@@ -28,11 +51,49 @@ public class SimpleClickFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //创建模拟数据
+        initData();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_simple_click, null);
+        }
+        unbinder = ButterKnife.bind(this, rootView);
+        //初始化适配器，监听器
+        initAdapter();
+        initListener();
+        return rootView;
+    }
+
+    private void initData() {
+        mList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            mList.add("item0" + i);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    private int i = 0;
+
+    //初始化监听器
+    private void initListener() {
+        //点击监听，添加了throttleFirst方法可以防抖动
+        //throttleFirst,为一段时间内仅能为第一次行为
+        RxView.clicks(clicksBtn).throttleFirst(600, TimeUnit.MILLISECONDS).subscribe(Void -> {
+            //Snackbar第一个参数可以是任意的view，他会往上找，最后找到rootView
+            Snackbar.make(clicksBtn, "发送了" + ++i + "个事件", Snackbar.LENGTH_SHORT).show();
+        });
+    }
+
+    //初始化适配器
+    private void initAdapter() {
     }
 }
