5d90df0c58225c5ba088b352e1366fe050933607
Goular <zhaojt_cs@126.com>
2017-04-09 12:33:20 +0800

    提交checkbutton的fragment的内容

--- app/src/main/java/com/goular/rx/rxuse/fragments/CheckboxFragment.java
+++ app/src/main/java/com/goular/rx/rxuse/fragments/CheckboxFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import com.goular.rx.R;
+import com.jakewharton.rxbinding.widget.RxCompoundButton;
 
 /**
  * Created by zhaoj on 2017/4/9.
@@ -14,6 +19,9 @@ import android.view.ViewGroup;
 public class CheckboxFragment extends Fragment {
 
     private static CheckboxFragment fragment;
+    private CheckBox signCB;
+    private Button signBtn;
+    private View rootView;
 
     public CheckboxFragment() {
         super();
@@ -33,6 +41,21 @@ public class CheckboxFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_checkbox, container, false);
+            signCB = (CheckBox) rootView.findViewById(R.id.sign_checkbox);
+            signBtn = (Button) rootView.findViewById(R.id.sign_btn);
+        }
+        //初始化监听器
+        initListener();
+        return rootView;
+    }
+
+    private void initListener() {
+        //选中事件监听
+        RxCompoundButton.checkedChanges(signCB).subscribe(bool -> {
+            signBtn.setEnabled(bool);
+            signBtn.setBackgroundResource(bool ? R.color.colorAccent : R.color.gray);
+        });
     }
 }
