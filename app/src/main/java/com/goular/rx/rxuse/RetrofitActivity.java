8bbd7113f641efe103e8e00cf3481f2d4839b714
Goular <zhaojt_cs@126.com>
2017-04-09 12:42:29 +0800

    提交内容

---
+++ app/src/main/java/com/goular/rx/rxuse/RetrofitActivity.java
@@ -0,0 +1,15 @@
+package com.goular.rx.rxuse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.goular.rx.R;
+
+public class RetrofitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit);
+    }
+}
