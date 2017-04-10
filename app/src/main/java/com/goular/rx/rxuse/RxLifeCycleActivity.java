94c764860391b327068999f5a69353b3712c4d89
Goular <zhaojt_cs@126.com>
2017-04-10 11:59:57 +0800

    添加RxLifeCycle的内容

---
+++ app/src/main/java/com/goular/rx/rxuse/RxLifeCycleActivity.java
@@ -0,0 +1,15 @@
+package com.goular.rx.rxuse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.goular.rx.R;
+
+public class RxLifeCycleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_life_cycle);
+    }
+}
